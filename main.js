232eb6f0a9f876937059cf99dd5df45411db65f3
joravv <joravulaa@gmail.com>
2025-11-27 20:49:47 +0100

    slider

---
+++ main.js
@@ -0,0 +1,10 @@
+var slides=document.getElementsByClassName("slide");
+var slideIndex=0;
+
+function showSlide(i){
+    slideIndex=(i+slides.length)%slides.length;
+    for(let s of slides)s.style.display="none";
+    slides[slideIndex].style.display="block"
+}
+showSlide(0);
+setInterval(()=> showSlide(slideIndex+1),2000)
